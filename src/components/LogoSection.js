c5fb6f9a58f1b1750a9a4e3641dc9c28bc4bdec4
pcion <patrick.ciongoli@gmail.com>
2023-11-07 18:35:19 -0800

    refactored homepage

---
+++ src/components/LogoSection.js
@@ -0,0 +1,41 @@
+// LogoSection.js
+import React from "react";
+import instagramReelsLogo from "../assets/image/instagram-reels-logo.png"; // Update the import paths as necessary
+import tiktokLogo from "../assets/image/tiktok-logo.png";
+import youtubeShortsLogo from "../assets/image/youtube-shorts-logo.png";
+
+const LogoSection = () => (
+   <div className="logos-section">
+      <a
+         href="https://www.youtube.com"
+         target="_blank"
+         rel="noopener noreferrer"
+      >
+         <img
+            src={youtubeShortsLogo}
+            alt="YouTube Shorts Logo"
+            className="logo"
+         />
+      </a>
+      <a
+         href="https://www.instagram.com"
+         target="_blank"
+         rel="noopener noreferrer"
+      >
+         <img
+            src={instagramReelsLogo}
+            alt="Instagram Reels Logo"
+            className="logo"
+         />
+      </a>
+      <a
+         href="https://www.tiktok.com"
+         target="_blank"
+         rel="noopener noreferrer"
+      >
+         <img src={tiktokLogo} alt="TikTok Logo" className="logo" />
+      </a>
+   </div>
+);
+
+export default LogoSection;
